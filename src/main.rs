0aecfac271d18814de787b0ac1c74009431aee13
distiny-cool <2442658787@qq.com>
2022-08-07 11:53:21 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let s = 95;
+    println!("Hello, world!");
+    println!("{:o}", s);
+    println!("{:X}", s);
+    println!("{:x}", s);
+    println!("{:p}", &s);
+    println!("{:#?}", ("test1", "test2"));
+}
